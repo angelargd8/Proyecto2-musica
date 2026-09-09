162d2eebf6fed5a0ac19b54ca142cec902719053
Angela Garcia <100092195+angelargd8@users.noreply.github.com>
2026-09-08 19:41:24 -0600

    cambio de nombres

--- Grupo3_EX3_PrototipoRitmico.py
+++ Grupo3_EX3_PrototipoRitmico.py
@@ -1,17 +1,21 @@
-# EX3.MC - Codigo base para percusion y bajo
+# EX3.MC - Prototipo ritmico: House
 from music import *
 
 # ---------- Configuracion general ----------
-tempo = 120
-repeticionesA = 4  # el patron de 2 compases sonara 4 veces
+tempo = 124
+repeticionesIntro = 4
+repeticionesA = 12
+repeticionesB = 8
+repeticionesAprima = 12
+repeticionesFinal = 4
 
-score = Score("Prototipo ritmico para P2.MC", tempo)
+score = Score("Prototipo ritmico house para P2.MC", tempo)
 
 # La percusion MIDI utiliza el canal 9.
-drumsPart = Part("Percusion", 0, 9)
+drumsPart = Part("Percusion house", 0, 9)
 
 # El bajo se mantiene en otra parte y otro canal.
-bassPart = Part("Bajo", ACOUSTIC_BASS, 1)
+bassPart = Part("Bajo house", ACOUSTIC_BASS, 1)
 
 
 def agregarPatron(frase, alturas, duraciones, repeticiones):
@@ -20,39 +24,109 @@ def agregarPatron(frase, alturas, duraciones, repeticiones):
         frase.addNoteList(alturas, duraciones)
 
 
-# ---------- Seccion A: ejemplo de 2 compases en 4/4 ----------
-bomboA = [BDR, REST] * 4
-durBomboA = [QN, QN] * 4
+def agregarSeccion(fraseBombo, fraseCaja, fraseHiHat, fraseBajo,
+                   bomboBase, cajaBase, hiHatBase, bajoBase,
+                   durPercusion, durBajo, repeticiones,
+                   abrirHiHat, golpeFinal, variarBajo):
+    """Agrega house con variacion ciclica de hi-hat y bajo."""
+    notasArmonia = [C2, E2, G2, A2]
+    for vuelta in range(repeticiones):
+        bombo = list(bomboBase)
+        caja = list(cajaBase)
+        hiHat = list(hiHatBase)
+        bajo = list(bajoBase)
 
-cajaA = [REST, SNR] * 4
-durCajaA = [QN, QN] * 4
+        if abrirHiHat and (vuelta + 1) % 4 == 0:
+            hiHat[15] = OHH
 
-hiHatA = [CHH] * 15 + [OHH]
-durHiHatA = [EN] * 16
+        if golpeFinal and vuelta == repeticiones - 1:
+            bombo[15] = BDR
 
-bajoA = [C2, REST, C2, E2, G2, REST, E2, G2]
-durBajoA = [QN] * 8
+        if variarBajo and vuelta % 2 == 0:
+            bajo[9] = notasArmonia[vuelta % len(notasArmonia)]
 
+        fraseBombo.addNoteList(bombo, durPercusion)
+        fraseCaja.addNoteList(caja, durPercusion)
+        fraseHiHat.addNoteList(hiHat, durPercusion)
+        fraseBajo.addNoteList(bajo, durBajo)
+
+
+# Cada patron dura 2 compases de 4/4: 16 corcheas.
+durDosCompases = [EN] * 16
+
+# ---------- Introduccion: solo pulso principal ----------
+bomboIntro = [BDR, REST, BDR, REST, BDR, REST, BDR, REST,
+              BDR, REST, BDR, REST, BDR, REST, BDR, REST]
+cajaIntro = [REST] * 16
+hiHatIntro = [REST, OHH, REST, OHH, REST, OHH, REST, OHH,
+              REST, OHH, REST, OHH, REST, OHH, REST, OHH]
+bajoIntro = [REST, C2, REST, C2, REST, C2, REST, C2,
+             REST, C2, REST, C2, REST, C2, REST, C2]
+
+# ---------- Seccion A: four-on-the-floor house ----------
+bomboA = [BDR, REST, BDR, REST, BDR, REST, BDR, REST,
+          BDR, REST, BDR, REST, BDR, REST, BDR, REST]
+cajaA = [REST, REST, SNR, REST, REST, REST, SNR, REST,
+         REST, REST, SNR, REST, REST, REST, SNR, REST]
+hiHatA = [CHH, OHH, CHH, OHH, CHH, OHH, CHH, OHH,
+          CHH, OHH, CHH, OHH, CHH, OHH, CHH, OHH]
+bajoA = [REST, C2, REST, C2, REST, E2, REST, G2,
+         REST, C2, REST, E2, REST, G2, REST, E2]
+
+# ---------- Seccion B: menos caja y mas espacio ----------
+bomboB = [BDR, REST, BDR, REST, BDR, REST, BDR, REST,
+          BDR, REST, BDR, REST, BDR, REST, BDR, REST]
+cajaB = [REST, REST, REST, REST, REST, REST, SNR, REST,
+         REST, REST, REST, REST, REST, REST, SNR, REST]
+hiHatB = [REST, OHH, CHH, OHH, REST, OHH, CHH, OHH,
+          REST, OHH, CHH, OHH, REST, OHH, CHH, OHH]
+bajoB = [REST, G2, REST, G2, REST, A2, REST, G2,
+         REST, E2, REST, E2, REST, G2, REST, A2]
+
+# ---------- Seccion A': regreso con bajo mas activo ----------
+bomboAprima = bomboA
+cajaAprima = cajaA
+hiHatAprima = hiHatA
+bajoAprima = [REST, C2, C2, REST, REST, E2, REST, G2,
+              REST, C2, E2, REST, REST, G2, A2, REST]
+
+# ---------- Ensamblaje ----------
 fraseBombo = Phrase(0.0)
 fraseCaja = Phrase(0.0)
 fraseHiHat = Phrase(0.0)
 fraseBajo = Phrase(0.0)
 
-agregarPatron(fraseBombo, bomboA, durBomboA, repeticionesA)
-agregarPatron(fraseCaja, cajaA, durCajaA, repeticionesA)
-agregarPatron(fraseHiHat, hiHatA, durHiHatA, repeticionesA)
-agregarPatron(fraseBajo, bajoA, durBajoA, repeticionesA)
+agregarPatron(fraseBombo, bomboIntro, durDosCompases, repeticionesIntro)
+agregarPatron(fraseCaja, cajaIntro, durDosCompases, repeticionesIntro)
+agregarPatron(fraseHiHat, hiHatIntro, durDosCompases, repeticionesIntro)
+agregarPatron(fraseBajo, bajoIntro, durDosCompases, repeticionesIntro)
 
+agregarSeccion(fraseBombo, fraseCaja, fraseHiHat, fraseBajo,
+               bomboA, cajaA, hiHatA, bajoA,
+               durDosCompases, durDosCompases, repeticionesA,
+               True, True, True)
 
-# ---------- Trabajo del equipo ----------
-# 1. Sustituir la seccion A por el patron del estilo investigado.
-# 2. Crear las secciones B y A' con cambios audibles.
-# 3. Incluir una variacion controlada mediante una regla,
-#    condicion o eleccion entre posibilidades definidas.
-# 4. Extender la estructura completa hasta superar 2 minutos.
+agregarSeccion(fraseBombo, fraseCaja, fraseHiHat, fraseBajo,
+               bomboB, cajaB, hiHatB, bajoB,
+               durDosCompases, durDosCompases, repeticionesB,
+               True, True, True)
 
+# Breve silencio antes del regreso de A'.
+fraseBombo.addNoteList([REST, REST], [QN, QN])
+fraseCaja.addNoteList([REST, REST], [QN, QN])
+fraseHiHat.addNoteList([REST, REST], [QN, QN])
+fraseBajo.addNoteList([REST, REST], [QN, QN])
 
-# ---------- Ensamblaje y exportacion ----------
+agregarSeccion(fraseBombo, fraseCaja, fraseHiHat, fraseBajo,
+               bomboAprima, cajaAprima, hiHatAprima, bajoAprima,
+               durDosCompases, durDosCompases, repeticionesAprima,
+               True, True, True)
+
+agregarSeccion(fraseBombo, fraseCaja, fraseHiHat, fraseBajo,
+               bomboA, cajaA, hiHatA, bajoA,
+               durDosCompases, durDosCompases, repeticionesFinal,
+               True, True, True)
+
 drumsPart.addPhrase(fraseBombo)
 drumsPart.addPhrase(fraseCaja)
 drumsPart.addPhrase(fraseHiHat)
@@ -63,4 +137,4 @@ score.addPart(bassPart)
 
 View.sketch(score)
 Play.midi(score)
-#Write.midi(score, "NombreEquipo_EX3_PrototipoRitmico.mid")
+Write.midi(score, "Grupo3_EX3_PrototipoRitmico.mid")
